942821c3f390fbc490ff98eea93da64ebbab2c5f
William Wong <ww8668@gmail.com>
2015-02-23 14:55:05 +0000

    Built JSX

--- react/build/cancer-type-selector.js
+++ react/build/cancer-type-selector.js
@@ -4,11 +4,21 @@ var BASE_URL = 'http://preview.macmillan.org.uk/';
 
 // Top-level component
 var CancerTypeSelector = React.createClass({displayName: "CancerTypeSelector",
+    getInitialState: function(){
+        return {
+            filterText: ''
+        }
+    },
+    handleUserInput: function(filterText) {
+        this.setState({
+            filterText: filterText
+        });
+    },
     render: function() {
         return (
             React.createElement("div", {className: "cancer-type-selector"}, 
-                React.createElement(CancerTypeSearchBar, null), 
-                React.createElement(CancerTypeList, {cancerTypes: this.props.cancerTypes})
+                React.createElement(CancerTypeSearchBar, {filterText: this.state.filterText, onUserInput: this.handleUserInput}), 
+                React.createElement(CancerTypeList, {cancerTypes: this.props.cancerTypes, filterText: this.state.filterText})
             )
         );
     }
@@ -16,10 +26,19 @@ var CancerTypeSelector = React.createClass({displayName: "CancerTypeSelector",
 
 // Second-level component
 var CancerTypeSearchBar = React.createClass({displayName: "CancerTypeSearchBar",
+    handleChange: function() {
+        this.props.onUserInput(
+            this.refs.cancerTypeSearchBar.getDOMNode().value
+        );
+    },
     render: function() {
         return (
             React.createElement("div", {className: "cancer-type-search-bar"}, 
-                React.createElement("input", {type: "text"})
+                React.createElement("input", {type: "text", placeholder: "Search for a cancer type", 
+                    value: this.props.filterText, 
+                    onChange: this.handleChange, 
+                    ref: "cancerTypeSearchBar"}
+                )
             )
         );
     }
@@ -29,8 +48,18 @@ var CancerTypeSearchBar = React.createClass({displayName: "CancerTypeSearchBar",
 var CancerTypeList = React.createClass({displayName: "CancerTypeList",
     render: function() {
         var rows = [];
+        var filterText = this.props.filterText;
+
         this.props.cancerTypes.forEach(function(cancer) {
-            rows.push(React.createElement(CancerItem, {cancer: cancer, key: cancer.id}));
+            if (cancer.name.indexOf(filterText) != -1) {
+                rows.push(React.createElement(CancerItem, {cancer: cancer, key: cancer.id}));
+            }
+
+            cancer.subtypes.forEach(function(subtype) {
+                if (cancer.name.indexOf(filterText) != -1) {
+                    rows.push(React.createElement(CancerItem, {cancer: subtype, key: subtype.id}));
+                }
+            });
         });
 
         return (
@@ -44,23 +73,13 @@ var CancerTypeList = React.createClass({displayName: "CancerTypeList",
 // Third-level component
 var CancerItem = React.createClass({displayName: "CancerItem",
     render: function() {
-        var rows = [];
-        this.props.cancer.subtypes.forEach(function(cancer) {
-            rows.push(React.createElement(CancerItem, {cancer: cancer, key: cancer.id}));
-        });
-
         return (
             React.createElement("li", {className: "cancer-item"}, 
                 React.createElement("span", {className: "item-name"}, 
                     React.createElement("a", {href: BASE_URL + this.props.cancer.slug}, 
                         this.props.cancer.name
                     )
-                ), 
-                rows.length ?
-                    React.createElement("ul", {className: "cancer-subtypes"}, 
-                        rows
-                    )
-                : null
+                )
             )
         );
     }
